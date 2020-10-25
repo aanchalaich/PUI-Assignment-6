23cac049591996549df7ba8da0b73e3b7c164ee4
aanchalaich <aanchalaich@gmail.com>
2020-10-25 16:50:16 -0400

    10/25 Change

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,9 +1,10 @@
 // 1. Create Buttons for All Options
 
 
+ 
 
-let colorButton=document.getElementsByClassName("Customization_Option");
-colorButton.onclick=changeColor() {
-    colorButton.style.backgroundColor= "#ffffff";
+function changeColor() {
+    alert('hello world');
+    document.getElementById("Customization_Option_One").style.background="#ff0000";
 }
 
