19debaf482d76731b24fbfec6c839fbd8a4ce878
aanchalaich <aanchalaich@gmail.com>
2020-10-25 23:48:43 -0400

    My buttonns store the value of each item

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,20 +1,57 @@
 // 1. Create Buttons for All Options
 
-currentColorSelection='Customization_Option_One'
+color_ids=['Customization_Option_One', 'Customization_Option_Two', 'Customization_Option_Three', 'Customization_Option_Four'];
+colors=["After-School Special", "Cozy Denim", "Rainy Day", "Morning Haze"];
+currentColorSelection='Customization_Option_One';
+currentColor="After-School Special";
 
 function changeColor(id) {
 
     let button=document.getElementById(id);
     
-     button.style.background="#ff0000";
+     button.style.background= "#99cccc";
 
     if (document.getElementById(currentColorSelection) != button) {
 
-        document.getElementById(currentColorSelection).style.background= "#fa8072";
+        document.getElementById(currentColorSelection).style.background= "white";
 
     }
 
     currentColorSelection=id;
+
+    for (let i=0; i<color_ids.length; i++) {
+        if (color_ids[i]==id) {
+            currentColor=colors[i];
+        }
+    }
+
+             
+}
+
+material_ids=['Customization_Option_Five', 'Customization_Option_Six', 'Customization_Option_Seven'];
+materials=["Hypoallergenic Poly Blend", "Memory Foam", "Duck Down"];
+currentMaterialSelection='Customization_Option_Five';
+currentMaterial="Hypoallergenic Poly Blend";
+
+function changeMaterial(id) {
+
+    let button=document.getElementById(id);
+    
+     button.style.background="#99cccc";
+
+    if (document.getElementById(currentMaterialSelection) != button) {
+
+        document.getElementById(currentMaterialSelection).style.background= "white";
+
+    }
+
+    currentMaterialSelection=id;
+
+    for (let i=0; i<material_ids.length; i++) {
+        if (material_ids[i]==id) {
+            currentMaterial=materials[i];
+        }
+    }
              
 }
 
